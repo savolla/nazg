3a48b5bce4de86461eba1d77589eb71efa991612
Kuzey Koç <kuzey.koc@kartaca.com>
2026-05-31 16:47:37 +0300

    xkarna: make more verbose directory structure and remove unsupported modules like programs from nix-on-droid

--- hosts/personal/mobile/a24/default.nix
+++ hosts/personal/mobile/a24/default.nix
@@ -1,80 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Simply install just the packages
   environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim # or some other editor, e.g. nano or neovim
-    neovim
-    tmux
+    # personal
+    vim neovim tmux git zoxide fish direnv starship
 
-    # Some common stuff that people expect to have
-    procps
-    killall
-    diffutils
-    findutils
-    util-linux
-    tzdata
-    hostname
-    man
-    gnugrep
-    gnupg
-    gnused
-    gnutar
-    bzip2
-    gzip
-    xz
-    zip
-    unzip
-    git
+    # essential
+    procps killall diffutils findutils util-linux
+    tzdata hostname man gnugrep gnupg gnused gnutar
+    bzip2 gzip xz zip unzip
   ];
 
-  # backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
 
-  # read the changelog before changing this value
   system.stateVersion = "24.05";
 
-  # set up nix for flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
-  # set your time zone
   time.timeZone = "Europe/Istanbul";
-
-  programs = {
-
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true; # use gpg key as your ssh key
-      pinentryPackage = pkgs.pinentry-curses;
-    };
-
-    tmux = {
-      enable = true;
-    };
-
-    ssh = {
-      enable = true;
-    };
-
-    fish = {
-      enable = true;
-    };
-
-    zoxide = {
-      enable = true;
-      enableFishIntegration = true;
-    };
-    direnv = {
-      enable = true;
-      enableFishIntegration = true;
-    };
-
-    # pretty shell
-    starship = {
-      enable = true;
-    };
   };
 }
