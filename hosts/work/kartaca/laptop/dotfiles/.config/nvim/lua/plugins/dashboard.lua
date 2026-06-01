b24b78ab1a5350d0b77d656a24344ac42f37a9e1
Kuzey Koç <kuzey.koc@kartaca.com>
2026-06-01 16:44:59 +0300

    finishing up stow dotfiles

--- hosts/work/kartaca/laptop/dotfiles/.config/nvim/lua/plugins/dashboard.lua
+++ hosts/work/kartaca/laptop/dotfiles/.config/nvim/lua/plugins/dashboard.lua
@@ -1,22 +1,21 @@
 return {
-  "snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        pick = function(cmd, opts)
-          return LazyVim.pick(cmd, opts)()
-        end,
-        header = [[
+	"snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				pick = function(cmd, opts)
+					return LazyVim.pick(cmd, opts)()
+				end,
+				header = [[
                         ██  ██        
                         ██  ██        
   ████████████  ██████████  ██  ██████
   ██  ██▄▄████  ████  ████  ██  ██▄▄██
 ████  ██  ██  ██  ████████████████  ██
       
-       we're just living to die       
 ]],
-        keys = {},
-      },
-    },
-  },
+				keys = {},
+			},
+		},
+	},
 }
